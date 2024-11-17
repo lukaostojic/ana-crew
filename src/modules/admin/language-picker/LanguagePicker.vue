8b7b9115c0192ecda2bff100710a0744a91b0e7c
Luka Ostojic <ostojaq842@gmail.com>
2024-11-17 20:02:32 +0100

    Fetch default language

--- src/modules/admin/language-picker/LanguagePicker.vue
+++ src/modules/admin/language-picker/LanguagePicker.vue
@@ -1,38 +1,41 @@
 <template>
-  <div class="dropdown-wrapper" ref="dropdown">
-    <div class="dropdown-header" @click="toggleDropdown">
+  <div v-show="!isLoading" class="dropdown__wrapper p-relative h-100 p-4" ref="dropdown">
+    <div class="dropdown__header d-flex justify-sb align-center" @click="toggleDropdown">
       <input
         type="text"
         placeholder="Search languages"
         v-model="searchQuery"
-        class="dropdown-search"
+        class="dropdown__search input w-100"
       />
       <span class="material-symbols-outlined dropdown-arrow" :class="{ open: isOpen }">
         keyboard_arrow_up
       </span>
     </div>
-    <ul v-if="isOpen" class="dropdown-list">
+    <ul v-if="isOpen" class="dropdown__list p-absolute">
       <li
         v-for="language in filteredLanguages"
         :key="language.code"
-        class="dropdown-item"
+        class="dropdown__item p-3"
         @click="selectLanguage(language)"
       >
         {{ language.name }}
       </li>
     </ul>
-    <div class="selected-languages">
-      <h4>Selected Languages:</h4>
+    <div class="mt-5">
+      <h4>Default Language</h4>
+      <p>{{ defaultLanguage?.name || 'No default set' }}</p>
+    </div>
+    <div class="mt-5">
+      <h4>Selected Languages</h4>
       <ul>
         <li v-for="language in selectedLanguages" :key="language.code">
           {{ language.name }}
         </li>
       </ul>
     </div>
-    <div class="default-language">
-      <h4>Default Language:</h4>
-      <p>{{ defaultLanguage?.name || 'No default set' }}</p>
-    </div>
+  </div>
+  <div class="dropdown__loader h-100 d-flex justify-center align-center" v-show="isLoading">
+    <img src="../../../assets/loading.gif" alt="loader" />
   </div>
 </template>
 
@@ -51,6 +54,7 @@ export default defineComponent({
     const availableLanguages = ref([...languages])
     const selectedLanguages = ref<{ code: string; name: string }[]>([])
     const defaultLanguage = ref<{ code: string; name: string } | null>(null)
+    const isLoading = ref(true)
 
     const fetchLanguages = async () => {
       const langRef = doc(db, 'languages', 'selectedLanguages')
@@ -69,9 +73,11 @@ export default defineComponent({
       }
 
       const settingsSnap = await getDoc(settingsRef)
+
       if (settingsSnap.exists()) {
         const settingsData = settingsSnap.data() as { code: string; name: string }
         defaultLanguage.value = settingsData
+        isLoading.value = false
       } else {
         console.log('No default language found!')
       }
@@ -117,10 +123,10 @@ export default defineComponent({
         ),
     )
 
-    const setAsDefaultLanguage = async (language: { code: string; name: string }) => {
-      defaultLanguage.value = language
-      await updateDefaultLanguageInFirebase(language)
-    }
+    // const setAsDefaultLanguage = async (language: { code: string; name: string }) => {
+    //   defaultLanguage.value = language
+    //   await updateDefaultLanguageInFirebase(language)
+    // }
 
     onMounted(() => {
       fetchLanguages()
@@ -140,7 +146,8 @@ export default defineComponent({
       selectedLanguages,
       defaultLanguage,
       dropdown,
-      setAsDefaultLanguage,
+      isLoading,
+      // setAsDefaultLanguage,
     }
   },
 })
